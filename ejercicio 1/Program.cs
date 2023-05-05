e2dcc2588c73a56908d8198ca975052bffcffefc
gisela387 <132630545+gisela387@users.noreply.github.com>
2023-05-05 16:02:27 -0600

    Add files via upload

---
+++ ejercicio 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vehiculo vehiculo = new vehiculo(01, "toyota", "Chevrolet", 7000, 12000, 2015);
+            Console.WriteLine("datos del vehiculo");
+            Console.WriteLine(vehiculo.ToString());
+            Console.ReadKey();
+        }
+    }
+}
