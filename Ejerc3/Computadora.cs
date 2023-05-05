3b8c0f258f66d2e0d6c5acf32fe083a78956c91b
gisela387 <132630545+gisela387@users.noreply.github.com>
2023-05-05 16:03:14 -0600

    Add files via upload

---
+++ Ejerc3/Computadora.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejerc3
+{
+    class Computadora
+    {
+            int capacidadDisco;
+            int espacioDispinible;
+            bool on;
+
+            public Computadora(int cpacidadDisco, int espacioDispinible, bool on)
+            {
+                this.CapacidadDisco = cpacidadDisco;
+                this.EspacioDispinible = espacioDispinible;
+                this.On = on;
+            }
+
+            public int CapacidadDisco { get => capacidadDisco; set => capacidadDisco = value; }
+            public int EspacioDispinible { get => espacioDispinible; set => espacioDispinible = value; }
+            public bool On { get => on; set => on = value; }
+
+            public void AñadirDato(int dato)
+            {
+                if (on)
+                {
+                    if (dato < espacioDispinible)
+                    {
+                        this.capacidadDisco += dato;
+                        Console.WriteLine("eEl dato se ha añadido");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("La computadora esta apagada");
+                }
+            }
+
+            public void EliminarDato(int dato)
+            {
+                if (on)
+                {
+                    if (dato < capacidadDisco)
+                    {
+                        capacidadDisco -= dato;
+                        Console.WriteLine("Se ha elimida el dato");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El dato es mayor a lo almacenado en el disco");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("La computadora esta apagada");
+                }
+            }
+            public void Enceder()
+            {
+                this.on = true;
+                Console.WriteLine("Encendiendo el equipo");
+            }
+            public void apagado()
+            {
+                this.on = false;
+                Console.WriteLine("Apagando el equipo");
+            }
+        }
+}
---
+++ Ejerc3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejerc3
+{
+    class Program
+    {
+
+        static void pedir(string m, out int n)
+        {
+            Console.WriteLine(m);
+            n = int.Parse(Console.ReadLine());
+        }
+        static void Menu()
+        {
+            Console.WriteLine("\n\n\tMENU\n");
+            Console.WriteLine("1.encender la computadora");
+            Console.WriteLine("2.anadir un dato");
+            Console.WriteLine("3.eliminar un dato");
+            Console.WriteLine("4.apagart la computadora");
+            Console.WriteLine("5.salir");
+        }
+        static void Ejecutar(Computadora cp)
+        {
+            int opc;
+            do
+            {
+                int dato;
+                Menu();
+                pedir("de la opcion: ", out opc);
+                Console.Clear();
+                switch (opc)
+                {
+                    case 1:
+                        cp.Enceder(); break;
+                    case 2:
+                        pedir("de el tamaño del dato: ", out dato);
+                        cp.AñadirDato(dato);
+                        break;
+                    case 3:
+                        pedir("de el tamaño del dato a eliminar: ", out dato);
+                        cp.EliminarDato(dato);
+                        break;
+                    case 4:
+                        cp.apagado();
+                        break;
+                    case 5:
+                        Console.WriteLine("saliendo...");
+                        break;
+                    default:
+                        Console.WriteLine("opcion fuera del rango");
+                        break;
+                }
+            }
+            while (opc != 5);
+        }
+        static void Main(string[] args)
+        {
+            Computadora cp = new Computadora(0, 200, false);
+            Ejecutar(cp);
+            Console.ReadKey();
+        }
+    }
+}
